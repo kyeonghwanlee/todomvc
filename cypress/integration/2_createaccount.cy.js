0d039d72c880d8f64365e0ddec1ac07aef4df54e
이경환 <111859180+kyeonghwanlee@users.noreply.github.com>
2022-09-13 14:32:39 +0900

    Update 2_createaccount.cy.js

--- cypress/integration/2_createaccount.cy.js
+++ cypress/integration/2_createaccount.cy.js
@@ -10,17 +10,17 @@ describe('empty spec', () => {
 
   it('2-1. createaccount failse (error email)', () => {
     // email text input false
-    cy.contains('이메일 형식으로 입력해주세요.').should('not.exist')
+    cy.get('.error-message').should('not.exist')
 
     cy.get('#id').type('admin')
     cy.get('.lk-button').click()
 
-    cy.contains('이메일 형식으로 입력해주세요.').should('exist') 
+    cy.get('.error-message').should('exist') 
   })
 
   it('2-2. createaccount failse(no password)', () => {
     // email text input false
-    cy.contains('영문, 숫자, 특수문자 조합 8자 이상으로 사용해주세요. 사용가능한 특수문자는 ~ ! @ # $ % ^ & * 입니다.').should('not.exist')
+    cy.get('.error-message').should('not.exist')
     cy.contains('약관에 동의하셔야 합니다.').should('not.exist')
 
     cy.get('#id').type('ruoghks@gmail.com')
@@ -28,20 +28,19 @@ describe('empty spec', () => {
     cy.get(':nth-child(4) > .lk-input > .lk-input-inner > input').type('admin')
     cy.get('.lk-button').click()
 
-    cy.contains('영문, 숫자, 특수문자 조합 8자 이상으로 사용해주세요. 사용가능한 특수문자는 ~ ! @ # $ % ^ & * 입니다.').should('exist')
-    cy.contains('약관에 동의하셔야 합니다.').should('exist') 
+    cy.get('.error-message').should('exist')
   })
 
   it('2-3. createaccount failse(no email & password)', () => {
     // email text input false
-    cy.contains('필수 정보입니다.').should('not.exist')
+    cy.get('.error-message').should('not.exist')
     cy.get('.lk-button').click()
-    cy.contains('필수 정보입니다.').should('exist') 
+    cy.get('.error-message').should('exist') 
   })
 
   it('2-4. createaccount failse(no checkbox)', () => {
     // email text input false
-    cy.contains('약관에 동의하셔야 합니다.').should('not.exist')
+    cy.get('.error-message').should('not.exist')
 
     cy.get('#id').type('ruoghks@gmail.com')
     cy.get(':nth-child(5) > .lk-input > .lk-input-inner > input').type('@dl29240730')
@@ -51,20 +50,20 @@ describe('empty spec', () => {
 
     cy.get('.lk-button').click()
     
-    cy.contains('약관에 동의하셔야 합니다.').should('exist') 
+    cy.get('.error-message').should('exist') 
   })
 
   it('2-5. createaccount failse(no checkbox)', () => {
     cy.get('#id').type('ruoghks@gmail.com')
     cy.get(':nth-child(5) > .lk-input > .lk-input-inner > input').type('@dl29240730')
     cy.get(':nth-child(4) > .lk-input > .lk-input-inner > input').type('이경환')
-    cy.get('[for="check_01"]').click()
 
-    cy.get('[class="require"]').eq(0).click()
+    cy.get('[style="margin-top: 14px;"] > .lk-checkbox > .checkmark').click()
+    cy.get('[style="margin-top: 19px;"] > .lk-checkbox > .checkmark').click()
 
     cy.get('.lk-button').click()
     
-    cy.contains('사용중인 이메일입니다.').should('exist') 
+    cy.get('.error-message').should('exist') 
   })
 
   it('2-6. createaccount ', () => {
